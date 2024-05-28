a9e455a07acb2df82a457c7031812ce63ac68fd1
TonyZhang112 <hz3719@nyu.edu>
2024-05-28 13:51:50 -0400

    modified

--- solutions.py
+++ solutions.py
@@ -11,6 +11,10 @@ def calculate_profit():
     You can assume the user will enter only numeric characters, e.g. "3000", not "$3,000.00"
     The output should match the format of the following examples: "Profit: $690.00" for sales of $3,000, or "Profit: $2,300.00" for sales of $10,000, etc.
     """
+    total_sales = float(input('what is your amount of total sales?'))
+    annual_profit = total_sales * 0.23
+    x = format(annual_profit, ',.2f')
+    print('Profit: $' + x+'\n')
 
 
 def calculate_quotient_and_remainder():
@@ -22,6 +26,11 @@ def calculate_quotient_and_remainder():
       Enter number #2: 2
       2 goes into 5 a total of 2 times with a remainder of 1
     """
+    number1 = int(input("What is your first number?"))
+    number2 = int(input('What is your second number?'))
+    quotient = number1 // number2
+    remainder = number1 % number2
+    print(str(number2) + ' goes into ' + str(number1) + " a total of "+ str(quotient)+ ' times with a remainder of '+ str(remainder)+'\n')
 
 
 def calculate_miles_per_gallon():
@@ -35,6 +44,25 @@ def calculate_miles_per_gallon():
       Gas used (gallons): 25
       Miles per gallon: 2.2
     """
+    print('This is a Miles Per Gallon (MPG) calculator.')
+    miles_driven= int(input("How many miles have you driven?"))
+    gas_used= int(input('How many gallons of gas you used?'))
+    mpg=int(miles_driven) / int(gas_used)
+    #print('Miles driven: '+ str(miles_driven) +'\nGas used (gallon): '+ str(gas_used) + '\nMiles per gallon: '+ str(mpg)+'\n')
+    x = format(miles_driven, '>25,.2f')
+    y = format(gas_used, '>20,.2f')
+    z = format(mpg, '>21,.2f')
+    a = format('Miles driven:', '<10')
+    b = format('Gas used (gallon):', '<10')
+    c = format('Miles per gallon:', '<10')
+
+    c1="{0}{1}".format(a,x)
+    c2="{0}{1}".format(b,y)
+    c3="{0}{1}".format(c,z)
+    
+    print(c1)
+    print(c2)
+    print(c3)
 
 
 def align_text():
@@ -53,3 +81,24 @@ def align_text():
       Price #2: $   10.00
       Price #3: $ 9532.60
     """
+    p1 = float(input("Please input 1st price value"))
+    p2 = float(input("Please input 2nd price value"))
+    p3 = float(input("Please input 3rd price value"))
+    x = format(p1, '>20,.2f')
+    y = format(p2, '>20,.2f')
+    z = format(p3, '>20,.2f')
+    a = format('price #1:', '<10')
+    b = format('price #2:', '<10')
+    c = format('price #3:', '<10')
+
+    c1="{0}{1}{2}".format(a,"$",x)
+    c2="{0}{1}{2}".format(b,"$",y)
+    c3="{0}{1}{2}".format(c,"$",z)
+    
+    print("Here are your prices!\n")
+    print(c1)
+    print(c2)
+    print(c3)
+
+
+      
